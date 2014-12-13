9c6f4dd984abe38a5f0f976418218d5d5696fbff
Greg Tarsa <gltarsa@gmail.com>
2014-12-13 00:47:50 -0500

    Added email.  Fixed bugs in code.

--- pick_secret_santa.rb
+++ pick_secret_santa.rb
@@ -1,8 +1,7 @@
+#!/Users/tarsa/.rbenv/shims/ruby
 require 'pry'
 
-names = %w( Greg Ben Linda Kat Becca )
-constraints = { "Linda" => "Greg", "Greg" => "Linda" }
-
+private
 def pick_partner(target, list, constraints)
 
   return if list.empty?
@@ -13,8 +12,61 @@ def pick_partner(target, list, constraints)
   partner
 end
 
-for name in names do
-  partner = pick_partner(name, names, constraints)
-  puts "#{name} is getting a gift for #{partner}"
-  names -= [partner]
+public
+# returns array [ { target: , recipient: , e-mail: }, ...]
+def pick_partners()
+  names = %w( Greg Ben Linda Kat Becca )
+  #emails = %w( gltarsa@gmail.com museiskum@gmail.com ltarsa@collegenannies.com kftarsa@gmail.com becca.tarsa@gmail.com )
+  emails = Array.new(names.length, "tarsa@nc.rr.com")
+
+  constraints = { "Linda" => "Greg", "Greg" => "Linda" }
+
+  list = []
+
+  names.each_with_index do |name, i|
+    if [name] == names # degenerate case: when giver == recipient, recurse.
+      return pick_partners()
+    end
+
+    partner = pick_partner(name, names, constraints)
+    names -= [partner]
+    list << { giver: "#{name}", recipient: "#{partner}", email: "#{emails[i]}" }
+  end
+  list
+end 
+
+dry_run = false
+assignments = pick_partners()
+subject = "You are officially a Secret Santa"
+
+assignments.each do |set|
+  msg_body =  <<-EOF
+        Dear Elf #{set[:giver]},
+
+        The Magic Selector has selected. You have been chosen to be Secret Santa for #{set[:recipient]}.
+
+        Your giving budget is $125.00
+
+        Have a Merry Christmas--and go make it a Merry Christmas for #{set[:recipient]}!
+
+        Warm Wishes,
+        Electric Santa
+  EOF
+
+  if dry_run
+    puts "msg_body=\n#{msg_body}----------\n" if set == assignments[0]
+    puts "would execute = %x( echo '<msg_body>' | mailx -s '#{subject}' #{set[:email]} )"
+  else
+    puts "Sending assignment to #{set[:email]}"
+    result = %x( echo '#{msg_body}' | mailx -s "#{subject}" #{set[:email]} )
+    status = $?
+      if status
+        puts "mail succeeded"
+        puts "result=#{result}" unless result.empty?
+    else
+      raise "Mail to elf #{name} failed.  Stopping e-mail to others."
+    end
+  end
 end
+
+
